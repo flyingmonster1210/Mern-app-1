09d3933ceb74aebda90f23f006582f9402b0c86b
Wayne <zweijie1210@outlook.com>
2023-07-18 16:04:07 -0700

    asyncHandler in backend controller

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,11 @@
 
-const getGoals = (req, res) => {
-  res.json({ message: 'get' })
-}
+const asyncHandler = require('express-async-handler')
 
-const setGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
+  res.json({ message: 'get' })
+})
+
+const setGoals = asyncHandler(async (req, res) => {
   console.log(req.body)
   if (!req.body.text) {
     res.status(400)
@@ -11,15 +13,15 @@ const setGoals = (req, res) => {
   }
 
   res.json({ message: 'post' })
-}
+})
 
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
   res.json({ message: `put with id = ${req.params.id}` })
-}
+})
 
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
   res.json({ message: `delete with id = ${req.params.id}` })
-}
+})
 
 module.exports = {
   getGoals,
