eda9dde61bd84a4013a0e06a4bb2b1f6fef8dae0
Wayne <zweijie1210@outlook.com>
2023-07-21 11:06:54 -0700

    connect MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT
 const app = express()
 
 const { errorHandler } = require('./middleware/errorMiddleware')
 const { requestInfo } = require('./middleware/normalMiddleware')
+const connetDB = require('./config/db')
+
+connetDB()
 
 app.listen(port, () => {
   console.log('\n\n-----------------------------------------------------------')
-  console.log(`Server is started at port ${port}\n`)
+  console.log(`Server is started at port ${port}`)
 })
 
 app.use(express.json())
