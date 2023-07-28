904fbadb5ab29d2c26b415b8696eaf8eebdc767c
Wayne <zweijie1210@outlook.com>
2023-07-27 17:09:59 -0700

    generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,7 +35,8 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     })
   } else {
     res.status(400)
@@ -47,9 +48,20 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/uses/login
 // @access  public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({
-    message: 'Login User'
-  })
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invild email or password')
+  }
 })
 
 // @desc    Get user data
@@ -60,6 +72,13 @@ const getME = asyncHandler(async (req, res) => {
     message: 'User data display'
   })
 })
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d'
+  })
+}
 
 module.exports = {
   registerUser,
