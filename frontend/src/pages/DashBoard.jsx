385b8b9430442ea52a59c16564b098327475e502
Wayne <zweijie1210@outlook.com>
2023-08-04 16:46:37 -0700

    Initial comment for component GoalForm

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import GoalForm from '../components/GoalForm'
 
 function DashBoard() {
   const navigate = useNavigate()
@@ -13,7 +14,15 @@ function DashBoard() {
     }
   }, [user, navigate])
 
-  return <div>DashBoard</div>
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+      <GoalForm />
+    </>
+  )
 }
 
 export default DashBoard
